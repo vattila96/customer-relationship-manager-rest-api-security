b3e2d60b09af47d475731d91bc1fda8e78bc03ce
Attila Varga <srep.ati@gmail.com>
2021-01-21 15:18:37 +0100

    Delete src/main directory

--- src/main/java/com/luv2code/springdemo/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.luv2code.springdemo.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-						extends AbstractSecurityWebApplicationInitializer {
-
-}
